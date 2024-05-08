301a6475a5d7188ded100fa5e93d96f0b5e1e1c9
saurabh-celes <98081549+saurabh-celes@users.noreply.github.com>
2024-05-08 18:30:48 +0530

    Feature/SUPPORT-2379 Updated react version to latest (#84)

--- example/index.js
+++ example/index.js
@@ -1,5 +1,11 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import ChartViewer from './ChartViewer';
 
-ReactDOM.render(<ChartViewer />, document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(
+  <React.StrictMode>
+    <ChartViewer />
+  </React.StrictMode>
+);
